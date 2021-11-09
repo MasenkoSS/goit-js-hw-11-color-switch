b6a07193aa040c0eea4d89992de3a0bc855105c2
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-11-09 16:53:58 +0300

    Update del (let random)

--- js/index.js
+++ js/index.js
@@ -15,7 +15,6 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-let random = null;
 let colorSwitch = null;
 refs.start.addEventListener("click", () => {
     changeStatusBtn (false, true);
